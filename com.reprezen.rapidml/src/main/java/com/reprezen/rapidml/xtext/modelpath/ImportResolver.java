0318e3ab64c873bd9ca65659ae9f67c43dc38f8b
Guillaume Hillairet <g.hillairet@gmail.com>
2019-01-17 17:09:22 +0100

    [ZEN-4674] Migrate RAPID-ML tests from API Studio to RAPID-ML project
    Small fixes to avoid NPE and implement DefaultRapidMLPreferences to be used in tests.

--- com.reprezen.rapidml/src/main/java/com/reprezen/rapidml/xtext/modelpath/ImportResolver.java
+++ com.reprezen.rapidml/src/main/java/com/reprezen/rapidml/xtext/modelpath/ImportResolver.java
@@ -82,7 +82,16 @@ public class ImportResolver {
         String template = item.getUriTemplate();
         try {
             return item.getMatches(fqModelName, importUri) //
-                    .map((ModelPathBindings b) -> b.interpolate(template != null ? template : containerUri.toString())) //
+                    .map((ModelPathBindings b) -> {
+                    	String value = null;
+                    	if (template != null) {
+                    		value = template;
+                    	} else if (containerUri != null) {
+                    		value = containerUri.toString();
+                    	}
+
+                    	return value != null ? b.interpolate(value) : null;
+                    }) //
                     .filter(Objects::nonNull) //
                     .map(wrapBadUri(uriString -> new URI(uriString))) //
                     .map(context -> importUri != null ? context.resolve(importUri) : context);
--- com.reprezen.rapidml/src/main/java/com/reprezen/rapidml/xtext/preferences/DefaultRapidMLPreferences.java
+++ com.reprezen.rapidml/src/main/java/com/reprezen/rapidml/xtext/preferences/DefaultRapidMLPreferences.java
@@ -8,18 +8,39 @@
  *******************************************************************************/
 package com.reprezen.rapidml.xtext.preferences;
 
+import java.util.Map;
+
+import javax.inject.Singleton;
+
 import org.eclipse.xtext.validation.SeverityConverter;
 
+import com.google.common.collect.Maps;
+
+@Singleton
 public class DefaultRapidMLPreferences implements RapidMLPreferences {
 
+	private final Map<String, Object> values = Maps.newHashMap();
+
 	@Override
-	public String getString(String name) {
-		return SeverityConverter.SEVERITY_IGNORE;
+	public String getString(String name) {		
+		if (values.containsKey(name)) {
+			return (String) values.get(name);
+		}
+
+		return SeverityConverter.SEVERITY_WARNING;
 	}
 
 	@Override
-	public boolean getBoolean(String name) {
-		return false;
+	public boolean getBoolean(String name) {		
+		if (values.containsKey(name)) {
+			return (boolean) values.get(name);
+		}
+
+		return true;
+	}
+
+	public void setValue(String key, Object value) {
+		values.put(key, value);
 	}
 
 }
